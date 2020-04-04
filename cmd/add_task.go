f3c6ad9a96fbf7d6012f8e9127f6b53e3fe4f5fe
Syauqi Rahmat Sugara <syauqi@sepulsa.com>
2020-04-04 21:58:00 +0700

    [feat]:connect to couchdb

--- cmd/add_task.go
+++ cmd/add_task.go
@@ -1,9 +1,12 @@
 package cmd
 
 import (
+	"context"
+
 	"github.com/spf13/cobra"
 
 	_ "github.com/go-kivik/couchdb" // The CouchDB driver
+	"github.com/go-kivik/kivik"
 )
 
 // globalDatatCmd represents the version command
@@ -11,7 +14,15 @@ var addTaskCmd = &cobra.Command{
 	Use:   "add-task",
 	Short: "Add task to list",
 	Run: func(cmd *cobra.Command, args []string) {
+		client, err := kivik.New("couch", "http://13.250.43.79:5984/")
+		if err != nil {
+			panic(err)
+		}
 
+		db := client.DB(context.TODO(), "tager")
+		if err != nil {
+			panic(err)
+		}
 	},
 }
 
