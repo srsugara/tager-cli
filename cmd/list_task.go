f6ac3a21386fbffa0b12eca315fad3ab81908db4
Syauqi Rahmat Sugara <syauqi@sepulsa.com>
2020-04-04 18:00:49 +0700

    [feat]:show data to table format

--- cmd/list_task.go
+++ cmd/list_task.go
@@ -3,7 +3,9 @@ package cmd
 import (
 	"context"
 
+	"github.com/fatih/color"
 	"github.com/go-kivik/kivik"
+	"github.com/rodaine/table"
 	"github.com/spf13/cobra"
 )
 
@@ -29,6 +31,11 @@ var listTaskCmd = &cobra.Command{
 		if err != nil {
 			panic(err)
 		}
+		// instantiate and styling table to show data
+		headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
+		columnFmt := color.New(color.FgYellow).SprintfFunc()
+		tbl := table.New("ID", "Title", "Description", "Status")
+		tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
 		for rows.Next() {
 			var doc interface{}
 			if err := rows.ScanDoc(&doc); err != nil {
@@ -36,7 +43,11 @@ var listTaskCmd = &cobra.Command{
 			}
 			/* do something with doc */
 			dat, _ := doc.(map[string]interface{})
-
+			tbl.AddRow(dat["_id"], dat["title"], dat["description"], dat["status"])
+		}
+		tbl.Print()
+		if rows.Err() != nil {
+			panic(rows.Err())
 		}
 	},
 }
